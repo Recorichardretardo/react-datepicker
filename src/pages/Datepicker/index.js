debd24074e1cd46db6c681ebc577a76d3c65c9ea
K Kasi <you@example.com>
2020-02-20 02:01:33 +0530

    react date picker examples

--- src/pages/Datepicker/index.js
+++ src/pages/Datepicker/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import getDate from "date-fns/getDate";
+import moment from 'moment';
 import './index.scss';
 //Clear datepicker input
 
@@ -11,33 +13,96 @@ import './index.scss';
     super();
     this.state = {
       name: 'React',
-      startDate:  null
+      startDate:  null,
+      endDate:null,
+      minDate:'01/09/2019',
+      maxDate:'07/09/2019'
     };
   }
   componentDidMount() {
    
+    //console.log("new date",new Date())
   }
 
   handleChange = date => {
+    console.log("date", moment(date).format('DD-MM-YYYY'))
+
+// moment(date).format('YYYY-MM-DD HH:MM:SS')
+
+
+   
     this.setState({
       startDate: date
     });
   };
 
+  endDateHandleChange = (date) => {
+    console.log("date", moment(date).format('DD-MM-YYYY'))
+
+    // moment(date).format('YYYY-MM-DD HH:MM:SS')
+    
+    
+       
+        this.setState({
+          endDate: date
+        });
+
+  }
+
   render() {
 
-   
+    const renderDayContents = (day, date) => {
+      const tooltipText = `Date: ${ moment(date).format('DD-MM-YYYY')}`;
+      return <span title={tooltipText}>{getDate(date)}</span>;
+    };
 
       
     return (<>
     <div className="date-picker-class">
      <DatePicker
-    
-        showPopperArrow={false}
+      todayButton={`Date: ${ moment(new Date('09-01-2019')).format('DD-MM-YYYY')} to  ${ moment(new Date('09-07-2019')).format('DD-MM-YYYY')} `}
+        dateFormat="dd-MM-yyyy"
         selected={this.state.startDate}
+        utcOffset={0}
+        minDate={new Date('09-01-2019')}
+        maxDate={new Date('09-07-2019')}
+       // selectsStart
+        openToDate={new Date('09-01-2019')}
         onChange={this.handleChange}
         isClearable
-        placeholderText="I have been cleared!"
+        placeholderText="DD-MM-YYYY"
+        renderDayContents={renderDayContents}
+        showMonthDropdown
+        useShortMonthInDropdown
+        //disabledKeyboardNavigation
+        tabIndex={1}
+        popperClassName="some-custom-class"
+        popperPlacement="top-end"
+        popperModifiers={{
+          offset: {
+            enabled: true,
+            offset: "5px, 10px"
+          },
+          preventOverflow: {
+            enabled: true,
+            escapeWithReference: false,
+            boundariesElement: "viewport"
+          }
+        }}
+      />
+</div>
+<div className="date-picker-class">
+     <DatePicker
+        dateFormat="dd-MM-yyyy"
+        selected={this.state.endDate}
+        utcOffset={0}
+        minDate={new Date('09-01-2019')}
+        maxDate={new Date('09-07-2019')}
+       // openToDate={new Date('09-07-2019')}
+       selectsEnd
+        onChange={this.endDateHandleChange}
+        isClearable
+        placeholderText="DD-MM-YYYY"
       />
 </div>
      </>);
